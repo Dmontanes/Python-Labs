459dab2b0a579ef25cddb30f325c72f35b7a4779
Dmontanes <dany_montan@hotmail.com>
2023-06-27 11:24:03 +0200

    cambios en lab 109

---
+++ LAB_109.py
@@ -0,0 +1,49 @@
+# LAB 109 - Tipos de datos numéricos
+
+# Ejercicio 1: Utilizar el shell de Python
+
+"""
+    En la pestaña de terminal, se puede iniciar un shell de Python escribiendo el comando:  
+        python3
+
+    Los tres simbolos "mayor que" (>>>) representan el símbolo del sistema donde el usuario puede escribir comandos de Python
+    Probamos:
+
+        Suma 2 + 2 
+        Resta 4 - 2
+        Multiplicación 2 * 2
+        División 4 / 2
+
+    Finalmente, salimos del shell de Python mediante el comando: 
+        quit () 
+"""
+
+# Ejercicio 2: Presentar el tipo de dato entero
+
+print ("Python tiene tres tipos numéricos: entero, flotante y complejo")
+
+""" 
+También se puede ejecutar en la consola con el comando:
+    python3 <filename>.py ; En este caso, python3 LAB_109.py
+"""
+
+    # Creación de una variable
+
+myValue = 1
+
+print (myValue)
+
+print (str(myValue) + " is of the data type " +str(type(myValue)))
+
+# Ejercicio 3: Presentar el tipo de dato flotante
+
+    # Para almacenar un número con decimales se debe definir como dato flotante
+
+myValue2 = 3.14
+
+print (myValue2)
+
+print (str(myValue2) + " is of the data type " +str(type(myValue2)))
+
+# Ejercicio 4: Presentar el tipo de dato complejo
+
