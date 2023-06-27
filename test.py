65bb6ca1652124b9b9ed741b4ddfc652f97db2d7
Dmontanes <dany_montan@hotmail.com>
2023-06-27 10:06:40 +0200

    bienvenida

---
+++ test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Test 01 Python
+"""
+print("Hello World!")
+name = "clouder"
+print("hola " + name)
+
+name2 = input ("¿Cómo te llamas? ")
+
+print (("Hola,"), name2,("¡Encantado de conocerte!"))
