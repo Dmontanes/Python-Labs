686ead7208154323108dd4fcc9bacf97efa604ce
Dmontanes <dany_montan@hotmail.com>
2023-06-27 10:28:57 +0200

    Labs 108 y 19

--- LAB 108.py
+++ LAB 108.py
@@ -1,8 +1,9 @@
 # LAB 108 - Hola Mundo
 
 # Ejercicio 1: Presentar Python
+    # Para confirmar la versión de Python escriba en el terminal: python --version
 
-    # para confirmar la versión de Python escriba en el terminal: python --version
+# Ejercicio 2: Escribir en Python
+    # Aprendiendo a programar, se suele comenzar con el programa Hello, World
 
-# Ejercicio 2: 
-
+print ("Hello, World")
